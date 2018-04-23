2f8b882749fbfa0fa1576391f024e9cfa9aedb1d
Tomas Härdin <tjoppen@acc.umu.se>
2018-04-23 11:17:50 +0200

    Add README, license python code under AGPLv3

---
+++ README.md
@@ -0,0 +1,17 @@
+# Solar panel stuff
+
+Most of this is in Swedish.
+
+## FreeCAD drawings
+
+Made using FreeCAD 0.16.
+
+## Insolation model
+
+Run modell/doit.m with octave.
+doit.sh can download data from other years or other coordinates.
+STRÅNG data for 2017 had some gaps, hence the scaling by 365/362 in doit.m.
+
+## License
+
+The Python code is released under AGPLv3 and the hardware is released under TAPRv1.
--- modell/doit.sh
+++ modell/doit.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# doit.sh - Downloads STRÅNG data from SMHI and computes how many kWh one solar panel produces per year
+# Copyright (C) 2018 Tomas Härdin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 set -e
 
 YEAR=2017
@@ -25,4 +41,5 @@ then
   wait $PIDS
 fi
 
+# Does not work right currently, year hardcoded in doit.m because I got annoyed
 octave --persist --no-gui doit.m -- $YEAR
